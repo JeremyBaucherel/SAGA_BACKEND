1f4741b951a36a51bf7f18033cd6238680b8e654
JeremyBaucherel <61932395+JeremyBaucherel@users.noreply.github.com>
2022-05-30 10:38:18 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ASGARD_BACKEND',
+    version='1.0.0',
+    description='HTTP daemon to serve SAGA BACKEND.',
+    url='https://github.com/JeremyBaucherel/SAGA/SAGA_BACKEND',
+    author='Jérémy BAUCHEREL',
+    author_email='jeremy.baucherel@free.fr',
+)
